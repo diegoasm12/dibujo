1c5cfb67c94395a8a4808de365d0e967a06ac399
diegoasm12 <102439978+diegoasm12@users.noreply.github.com>
2022-09-07 20:32:46 +0200

    Add files via upload

---
+++ dick.py
@@ -0,0 +1,19 @@
+import turtle
+
+fondo = turtle.Screen()
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(25)
+turtle.right(30)
+turtle.forward(125)
+turtle.right(90)
+turtle.forward(-10)
+turtle.circle(30)
+turtle.forward(100)
+turtle.circle(29)
+turtle.right(90)
+turtle.forward(125)
+turtle.right(60)
+turtle.forward(35)
+
+turtle.exitonclick()
